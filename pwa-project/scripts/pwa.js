8d0759718bc3c7e71965d7f90c26bb98756fb3c5
nvhiii <nahikhan16@gmail.com>
2024-05-19 19:10:35 -0400

    pwa service worker and appjs added

---
+++ pwa-project/scripts/pwa.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll([
+        "../html/index.html",
+        "../html/manifest.json",
+        "./app.js",
+        "./pwa.js",
+        "../html/icon-192x192.png",
+        "../html/icon-512x512.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
